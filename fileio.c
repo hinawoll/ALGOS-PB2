c05d60b1f36ac2a62329c0333e36e7d2de5b296e
Michael Souhrada <michaelsouh@gmail.com>
2026-04-18 15:31:34 +0200

    implemented fileio

---
+++ fileio.c
@@ -0,0 +1,62 @@
+#include "fileio.h"
+#include <string.h>
+#include <stdlib.h>
+
+void menu()
+{
+    printf("treecheck: \n");
+}
+
+tnode* loadtree(char* dateiname, tnode* root, int entries)//entries ist die anzahl der einträge des baumes
+{
+    FILE* file;
+    int key = 0;
+    file = fopen(dateiname, "r");
+    if(file == NULL)
+    {
+        printf("Datei konnte nicht geöffnet werden!\n");
+        return root;
+    }
+    else if(entries > 0)
+    {
+        freeTree(root);//wenn der baum einträge hat dann wird er resetted
+    }
+
+    while(fscanf(file, "%d", &key) != EOF)
+    {
+        root = insertNode(root, key);
+    }
+    fclose(file);
+
+    return root;
+}
+
+void minmaxavg(tnode* root, int* minimum, int* maximum, double* sum, double* counter)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+
+
+    minmaxavg(root->left, minimum, maximum, sum, counter);
+
+    if (root->key < *minimum)
+        *minimum = root->key;
+
+    if (root->key > *maximum)
+        *maximum = root->key;
+
+    *sum = *sum + root->key;
+    *counter = *counter + 1;
+
+
+    minmaxavg(root->right, minimum, maximum, sum, counter);
+}
+
+
+
+
+
+
+
